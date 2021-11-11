011839233eca65f0e0d2c04639463b8a2e466447
l_heid12 <l_heid12@uni-muenster.de>
2021-11-11 13:06:34 +0100

    extended requirements.txt

---
+++ Backend/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def main():
+    testDF = pd.DataFrame(
+        {
+            'abc': [1, 2, 3],
+            'def': [4, 5, 6]
+        }
+    )
+
+    print('Hello world')
+
+
+if __name__ == '__main__':
+    main()
