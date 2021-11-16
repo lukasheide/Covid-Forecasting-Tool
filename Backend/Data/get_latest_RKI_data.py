353d9b9136d0dab26f3a31e1bd5db9a60cc9492a
l_heid12 <l_heid12@uni-muenster.de>
2021-11-16 10:54:31 +0100

    added directory where to store csv and made script executable

--- Backend/Data/get_latest_RKI_data.py
+++ Backend/Data/get_latest_RKI_data.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import time
 
 
-def create_csv_with_latest_rk_data():
+def create_csv_with_latest_rki_data():
     rki_res = req.get(
         'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_history_hubv/FeatureServer/0/query?where=%20(AdmUnitId%20%3D%205515%20OR%20AdmUnitId%20%3D%205516)%20&outFields=*&outSR=4326&f=json')
     data = rki_res.json()
@@ -21,4 +21,8 @@ def create_csv_with_latest_rk_data():
 
     datetime.today().strftime('%d%m%y')
 
-    dataRKI.to_csv(datetime.today().strftime('%d%m%y')+'.csv')
+    dataRKI.to_csv('./../Assets/Data/rki_data_'+datetime.today().strftime('%d%m%y')+'.csv')
+
+
+if __name__ == '__main__':
+    create_csv_with_latest_rki_data()
