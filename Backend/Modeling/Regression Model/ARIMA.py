803dde0e697e091fe662abd8ac6ecefa19ed6c8a
Toni <a_beck54@uni-muenster.de>
2022-01-09 15:43:21 +0100

    Using compute_evaluation_metrics for error measures

--- Backend/Modeling/Regression Model/ARIMA.py
+++ Backend/Modeling/Regression Model/ARIMA.py
@@ -2,34 +2,24 @@ import pandas as pd
 import numpy as np
 import pmdarima as pmd
 from statsmodels.tsa.stattools import adfuller
-from sklearn.metrics import mean_squared_error
-from math import sqrt
 from Backend.Visualization.modeling_results import plot_sarima_model_line_plot
 from Backend.Evaluation.metrics import compute_evaluation_metrics
-import matplotlib.pyplot as plt
 
 #reading csv file including date, cases, class
 df = pd.read_csv("data_smoothed.csv")
-print(df.shape)
-print(df.info())
 
 #creating an array with the dates
 date = df["Date"]
-print(date.shape)
 
 #creating test and training data set
 train = df[df["Class"] == "train"]
 test = df[df["Class"] == "test"]
-print(train.shape)
-print(test.shape)
 
 #create training array
 train_array = train["Cases"]
-print(train_array.shape)
 
 #create test array
 test_array = test["Cases"]
-print(test_array.shape)
 
 print("p-value:", adfuller(train_array.dropna())[1])
 
@@ -52,29 +42,6 @@ def sarimamodel(timeseriesarray, i):
                                          stepwise=True, test = "adf")
         return autoarima_model
 
-#creating ARIMA model
-def arimamodel(timeseriesarray):
-    autoarima_model = pmd.auto_arima(timeseriesarray,
-                              start_p=1,
-                              start_q=1,
-                                     max_p=3,
-                                     max_q=3,
-                              test="adf",
-                             trace=True)
-
-    return autoarima_model
-
-#evaluating arima model
-def mean_absolute_percentage_error(y_true, y_pred):
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
-#evaluating arima model
-def root_mean_square_error(y_true, y_pred):
-    return sqrt(mean_squared_error(y_true, y_pred))
-
-
-
 #model execution
 def sarima_pipeline(train_array, test_array):
     m = 2000
@@ -85,16 +52,15 @@ def sarima_pipeline(train_array, test_array):
         arima_model = sarimamodel(train_array, i)
         predictions = arima_model.predict(len(test_array))
         evaluations = compute_evaluation_metrics(test_array, predictions)
-        print(evaluations)
-        MAPE = mean_absolute_percentage_error(test_array, predictions)
-        RMSE = root_mean_square_error(test_array, predictions)
+        MAPE = evaluations["mape"]
+        RMSE = evaluations["rmse"]
         print(MAPE)
         print(RMSE)
 
         #save best fitting model
         if MAPE < m:
-            m = mean_absolute_percentage_error(test.Cases, predictions)
-            r = root_mean_square_error(test.Cases, predictions)
+            m = MAPE
+            r = RMSE
             final_model = arima_model
         i +=1
 
