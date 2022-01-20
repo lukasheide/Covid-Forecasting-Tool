c2568e27c3d050e108020b996824abebdde655d5
Lasitha Uyangodage <luyangod@uni-muenster.de>
2022-01-20 22:03:45 +0100

    DB file transfer progress bar added

--- Backend/Data/DataManager/remote_db_manager.py
+++ Backend/Data/DataManager/remote_db_manager.py
@@ -1,4 +1,6 @@
 from paramiko import Transport, SFTPClient
+
+from Backend.Data.DataManager.data_util import print_progress
 from Backend.Data.DataManager.properties import Server
 import shutil
 
@@ -16,7 +18,8 @@ def download_db_file():
     remote_path = "Assets/Databasefile/opendaten.db"
     local_path = "../Assets/Data/opendaten.db"
 
-    sftp.get(remotepath=remote_path, localpath=local_path)
+    print("downloading the database file from the server:")
+    sftp.get(remotepath=remote_path, localpath=local_path, callback=print_progress)
 
     sftp.close()
     transport.close()
@@ -35,7 +38,8 @@ def upload_db_file():
     remote_path = "Assets/Databasefile/opendaten.db"
     local_path = "../Assets/Data/opendaten.db"
 
-    sftp.put(remotepath=remote_path, localpath=local_path)
+    print("uploading the database file to the server:")
+    sftp.put(remotepath=remote_path, localpath=local_path, callback=print_progress)
 
     sftp.close()
     transport.close()
