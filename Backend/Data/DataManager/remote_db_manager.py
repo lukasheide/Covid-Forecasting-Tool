fde46c058915533724c6be30e7ec039c23613e48
Lasitha Uyangodage <luyangod@uni-muenster.de>
2022-01-30 20:57:27 +0100

    pred_intervals added to remote_db_manager

--- Backend/Data/DataManager/remote_db_manager.py
+++ Backend/Data/DataManager/remote_db_manager.py
@@ -53,12 +53,61 @@ def upload_db_file():
     transport.close()
 
 
+def download_pred_intervals_file():
+    transport = Transport((Server.host, Server.port))
+    transport.connect(username=Server.username, password=Server.password)
+    sftp = SFTPClient.from_transport(transport)
+
+    # create a copy of the existing database file
+    original = '../Assets/Data/prediction_intervals.csv'
+    target = '../Assets/Data/prediction_intervals_backup.csv'
+    shutil.copy2(original, target)
+
+    remote_path = "Assets/DataUtils/prediction_intervals.csv"
+    local_path = "../Assets/Data/prediction_intervals.csv"
+
+    print("downloading the prediction_intervals file from the server:")
+    sftp.get(remotepath=remote_path, localpath=local_path, callback=print_progress)
+
+    sftp.close()
+    transport.close()
+
+
+def upload_pred_intervals_file():
+    transport = Transport((Server.host, Server.port))
+    transport.connect(username=Server.username, password=Server.password)
+    sftp = SFTPClient.from_transport(transport)
+
+    # create a local copy of the existing database file
+    original = '../Assets/Data/prediction_intervals.csv'
+    target = '../Assets/Data/prediction_intervals_backup.csv'
+    shutil.copy2(original, target)
+
+    remote_path = "Assets/DataUtils/prediction_intervals.csv"
+    local_path = "../Assets/Data/prediction_intervals.csv"
+
+    print("uploading the prediction_intervals file to the server:")
+    sftp.put(remotepath=remote_path, localpath=local_path, callback=print_progress)
+
+    timestamp = datetime.now()
+    remote_path_backup = f"Assets/DataUtils/prediction_intervals_{timestamp}.csv"
+    sftp.put(remotepath=remote_path_backup, localpath=local_path)
+
+    # save backup
+
+    sftp.close()
+    transport.close()
+
 
 if __name__ == '__main__':
 
-    task = 'upload'
+    task = 'download'
 
     if task == 'upload':
         upload_db_file()
     elif task == 'download':
         download_db_file()
+    elif task == 'upload_pred_intervals':
+        upload_pred_intervals_file()
+    elif task == 'download_pred_intervals':
+        download_pred_intervals_file()
