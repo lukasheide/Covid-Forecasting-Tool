d0311b052c31353b1ce70fd6a54c8f98dba9d064
l_heid12 <l_heid12@uni-muenster.de>
2022-02-21 16:37:39 +0100

    added a lot of comments for describing the functions in detail

--- Backend/Modeling/Differential_Equation_Modeling/model_params.py
+++ Backend/Modeling/Differential_Equation_Modeling/model_params.py
@@ -1,41 +1,50 @@
 import numpy as np
 
+#######################################################################################################################
+# Below the values of each parameter of the differential equation model can be adjusted.
+# Here only the mean-value and thus the point estimate of each model parameter is used in the end.
+# Other values related to upper and lower limits, variances, as well as distributions could be used to introduce
+# stochasticity. We decided against that here, as we instead opted for a so-called bootstrapping approach to provide
+# predictions intervals instead of using a simulation in which for each model run all parameters are randomly drawn
+# and then the same model is rerun n times and the average values are used as point estimates.
+#######################################################################################################################
+
 params_SEIRV_fixed = {
     'gamma_I': { # mean infectious period detected cases
        'mean': 1 / 3.6,
-        'sd': 1 / 3.6 / 10,
-        'upper_lim': 2,
-        'lower_lim': 0,
-        'distribution': "normal"
+        'sd': 1 / 1.85,
+        'upper_lim': 1 / 2.1,
+        'lower_lim': 1 / 10,
+        'distribution': "lognormal"
     },
 
     'gamma_U': {  # mean infectious period undetected cases
         'mean': 1 / 2.1,
-        'sd': 1 / 2.1 / 10,
-        'upper_lim': 2,
-        'lower_lim': 0,
+        'sd': 1 / 1.1,
+        'upper_lim': 1 / 1.4,
+        'lower_lim': 1 / 5.88,
         'distribution': "normal"
     },
 
     'delta': {  # mean incubation time
         'mean': 1 / 2,
-        'sd': 1 / 2 / 10,
+        'sd': 1 / 2 / 10, # placeholder
         'upper_lim': 1 / 5,
         'lower_lim': 1 / 1.5,
         'distribution': "normal"
     },
 
-    'theta': {  # vaccination protection #### OVERWRITTEN BY COMPUTED EFFICIENCY
+    'theta': {  # vaccination protection #### OVERWRITTEN BY COMPUTED VACCINATION EFFICIENCY (-> no longer used)
         'mean': 0.1,
-        'sd': 0.1 / 10,
+        'sd': 0.1 / 10, # placeholder
         'upper_lim': 1,
         'lower_lim': 0,
         'distribution': "normal"
     },
 
-    'rho': {  # probability related to ratio of undetected cases (Dunkelziffer)
-        'mean': (1.35 - 1) / 1.35,
-        'sd': (1.35 - 1) / 1.35 / 10,
+    'rho': {  # probability related to ratio of undetected cases (Undetected Ratio)
+        'mean': (1.35 - 1) / 1.35,                  # Transformation from Ratio to probability
+        'sd': (1.35 - 1) / 1.35 / 10, # placeholder
         'upper_lim': 1,
         'lower_lim': 0,
         'distribution': "normal"
@@ -54,13 +63,24 @@ params_SEIRV_fit = {
 }
 
 
+# Parameter is used for fitting the differential equation model to historical data. The model fitting is done by
+# adjusting the fitted parameters such that the difference between the resulting predicted infections and historical
+# infections is minimized. This is done by computing the Mean-Squared-Errors. We additionally weighted these errors
+# and assigned more weight to residuals towards the end of the fitting period. This ensures that large deviations
+# are penalized more strongly shortly before the forecasting period starts compared to the point that is furthest
+# away from the forecasting period.
 LEAST_SQUARES_WEIGHT = {
-    'lambda': 0.05, # exponential decay
+    'lambda': 0.05,          # exponential decay
     'lambda_inverse': -0.02
 }
 
 
 def draw_value_from_param_distribution(paramname):
+    """
+    This function did not end up being used in our final modeling infrastructure. More details why and can be found
+    above. The aim of this function was to draw random parameter values for introducing stochasticity. 
+    """
+
     # get information for parameter:
     param = params_SEIRV_fixed[paramname]
 
@@ -73,7 +93,7 @@ def draw_value_from_param_distribution(paramname):
         )
 
     # Lognormal
-    elif param['distribution'] == 'uniform':
+    elif param['distribution'] == 'lognormal':
         random_val = np.random.lognormal(
             loc=param['mean'],
             scale=param['sd']
